b51ffb8f750719b2bb68bce826f63d098acd0326
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-02-22 10:45:27 +0100

    added pretty road

---
+++ project/scripts/env/road.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+const SPEED : float = 100;
+const LIMIT : float = -320;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	for portion in get_tree().get_nodes_in_group("road") :
+		portion.global_position.z -= SPEED * delta
+		if portion.global_position.z < LIMIT :
+			portion.global_position.z += -LIMIT * 2
