7f492ec83b2651fd1e0035f44b9c54d0d263ce13
eliott.jouvin <eliott.jouvin@2025.icam.fr>
2024-02-22 16:48:09 +0100

    Add Game over by speed limit

--- project/scripts/levels/game.gd
+++ project/scripts/levels/game.gd
@@ -5,19 +5,33 @@ extends Node3D
 @export var Score : Node
 @export var GameOver : Node
 
+@export var min_car_velocity : int = 30  # valeur à ajuster
+@export var max_car_velocity : int = 70
+@export var chance_update_velocity : float = 0.01 # probabilité de chnager
+@export var velocity_boundary : int = 10
+@export var weight_velocity : float = 0.05
+
 var score : float = 0
+var car_velocity : float = 50   # vitesse de la voiture en km/h
+var car_target_velocity : int = 40
+
 
 @onready var Transition : Node = get_node("/root/SceneTransition")
 
 func _process(delta : float) -> void :
-	Debug.print_left("Press F3 to close the debug menu", 1)
-	Debug.print_right("FPS : " + str(round(1/delta)), 0)
-	
 	score += delta * 2.5
-	
 	Score.text = "Score : " + "0".repeat(8 - floor(log(max(1,score*5))/log(10))) + str(floor(score)*5)
 	
-	Debug.update_text()
+	if randf() < chance_update_velocity:
+		car_target_velocity = randi_range(min_car_velocity-velocity_boundary, max_car_velocity+velocity_boundary)
+	
+	if car_velocity > car_target_velocity + weight_velocity:
+		car_velocity -= weight_velocity
+	elif car_target_velocity - weight_velocity > car_velocity :
+		car_velocity += weight_velocity
+	
+	check_4_kill()
+	debug(delta)
 
 
 func die(reason: String) -> void :
@@ -27,14 +41,20 @@ func die(reason: String) -> void :
 	Score.hide()
 	get_tree().paused = true
 	
-
+func check_4_kill() -> void:
+	if car_velocity > max_car_velocity:
+		die("You were going above the speed limit: " + str(max_car_velocity)+ " km/h (fuck USA with mph)\nThe police caught you !")
+	if car_velocity < min_car_velocity:
+		die("You were going under the speed limit: " + str(min_car_velocity)+ " km/h (fuck USA with mph)\nThe baby shat himself.")
 
 func _on_brakes_clicked() -> void :
-	print("brakes")
+	var brake : int = randi_range(0,10)
+	car_velocity -= brake
 
 
 func _on_gaz_clicked() -> void :
-	print("gaz")
+	var speed_up : int = randi_range(0,10)
+	car_velocity += speed_up
 
 
 func _on_honk_clicked() -> void :
@@ -54,3 +74,10 @@ func _on_game_over_restart() -> void:
 func _on_baby_clicked() -> void :
 	print("baby touched")
 	die("you touched the baby")
+
+func debug(delta) -> void:
+	Debug.print_left("Press F3 to close the debug menu", 1)
+	Debug.print_left("Car velocity " + str(round(car_velocity)) + " km/h", 2)
+	Debug.print_left("Car target velocity " + str(round(car_target_velocity)) + " km/h", 3)
+	Debug.print_right("FPS : " + str(round(1/delta)), 0)
+	Debug.update_text()
