ccf9895de3e0d4807eadad159c85c2054aa8f409
WanderingPenwing <nicolas.pinson31@gmail.com>
2024-02-25 09:51:03 +0100

    smoother steering

--- project/scripts/entities/steering.gd
+++ project/scripts/entities/steering.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 const TURNING_SPEED : float = 2.4
+const INERTIA : float = 0.999
 
 @export var Wheel : Node
 
@@ -13,7 +14,7 @@ var wheel_angle : float = 0
 func _process(_delta: float) -> void:
 	Wheel.rotation_degrees.z += max(min(wheel_angle - Wheel.rotation_degrees.z, TURNING_SPEED), -TURNING_SPEED)
 	wheel_angle += max(min(0 - wheel_angle, 0.5 * TURNING_SPEED), - 0.5 * TURNING_SPEED)
-	World.global_position.x = Wheel.rotation_degrees.z * 0.1
+	World.global_position.x = lerp(World.global_position.x, Wheel.rotation_degrees.z * 0.1, INERTIA)
 	
 	if World.global_position.x < -10 :
 		Game.die("Maybe a bit too much left no ?... it is not even the right side of the road...",4)
